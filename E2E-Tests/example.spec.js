30a28cfdc37e13d27cdd24f5bd758f35c54d2de5
joelandrews614 <joelandrews614@gmail.com>
2025-03-24 18:13:07 +0530

    Three Questions

--- E2E-Tests/example.spec.js
+++ E2E-Tests/example.spec.js
@@ -10,6 +10,40 @@ test("Exercise - 1: Take Screenshot of a the webpage", async ({page}) => {
 
 test("Exercise - 2: File Uploads", async ({page}) => {
 
-  
+  await page.goto("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
+  await page.locator("//input[@id='picture']").setInputFiles("testFiles\\JoelAndrewsJeyakumar_QA_Automation.pdf");
 
+})
+
+test("Exercise - 3: Frame Handling", async ({page}) => {
+
+  await page.goto("https://www.hyrtutorials.com/p/frames-practice.html");
+  
+  const frame2 = await page.frame({name: 'frm2'});
+  if (frame2) {
+    await frame2.fill("//input[@id='firstName']", "Joel Andrews Jeyakumar");
+  } else {
+    console.error("Frame 'frm2' not found");
+  }
+})
+
+test("Exercise - 4: Outlook", async ({page}) => {
+
+  await page.goto("http://outlook.office.com/");
+  
+  await page.getByPlaceholder("Email, phone, or Skype").fill("joel@vivyacorp.com");
+  
+  await page.getByText("Next").click();
+  
+  await page.locator("//input[@name='passwd']").fill("vrvictory!6");
+
+  await page.locator("//input[@type='submit']").click();
+
+  await page.locator("//input[@type='button']").click();
+
+  await page.locator("(//div[@title='ValianttInfo - 3 items (0 unread)'])[1]").click();
+
+  await page.locator("(//span[@title='testuser@valianttinfo.com'])[1]").click();
+
+  await expect(page.locator("//p[3]//b[1]")).toHaveText("Candidate's Name: k");
 })
